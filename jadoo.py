a6e6d9776d0f8d13ea496232ac50875cdfedb613
Bill <bill@timepercentcorp.com>
2023-02-17 12:18:42 +0900

    feat: add trimmer for jadoo lang
    add trimmer module to delete in blank, break line and double quotes in tokenized code

--- jadoo.py
+++ jadoo.py
@@ -4,7 +4,6 @@ from enum import Enum, auto
 
 class TokenType(Enum):
     '''token type of jadoo lang'''
-
     # mark
     PAREN_OPEN = auto()
     PAREN_CLOSE = auto()
@@ -13,7 +12,6 @@ class TokenType(Enum):
     MINUS = auto()
     MULTIPLE = auto()
     DIVISION = auto()
-    SINGLE_QUOTES = auto()
     DOUBLE_QUOTES = auto()
     BLANK = auto()
     BREAKLINE = auto()
@@ -33,6 +31,7 @@ class TokenType(Enum):
     NONE = auto()
     TEMP = auto()
     VARNAME = auto()
+    ROOT = auto()
 
 mark_literal : list[str] = ["" for i in range(len(TokenType)+1)]
 mark_literal[TokenType.PAREN_OPEN.value] = "("
@@ -42,7 +41,6 @@ mark_literal[TokenType.PLUS.value] = "+"
 mark_literal[TokenType.MINUS.value] = "-"
 mark_literal[TokenType.MULTIPLE.value] = "*"
 mark_literal[TokenType.DIVISION.value] = "/"
-mark_literal[TokenType.SINGLE_QUOTES.value] = "'"
 mark_literal[TokenType.DOUBLE_QUOTES.value] = '"'
 mark_literal[TokenType.BLANK.value] = " "
 mark_literal[TokenType.BREAKLINE.value] = "\n"
@@ -57,22 +55,19 @@ keyword_literal[TokenType.DOO.value] = "DOO"
 @dataclass
 class Token():
     '''token class of jadoo lang'''
-
     def __post_init__(self):
         if mark_literal[self.type.value] != "":
             self.literal = mark_literal[self.type.value]
         elif keyword_literal[self.type.value] != "":
             self.literal = keyword_literal[self.type.value]
 
-    type : TokenType = None
+    type : TokenType = TokenType.NONE
     literal : str = None
     value : any = None
 
-# lexer
-def lexer(line : str) -> list:
+def lexer(line : str) -> list[Token]:
     '''lexer of jadoo lang'''
-
-    tokenized : list = []
+    tokenized : list[Token] = []
     tmp : str = ""
     i = 0
     while i < len(line):
@@ -121,44 +116,110 @@ def lexer(line : str) -> list:
         tmp = ""
     return tokenized
 
+def trimmer(tokenized : list[Token]) -> list[Token]:
+    '''trimmer of jadoo lang'''
+    trimmed : list[Token] = []
+    for tkn in tokenized:
+        if tkn.type == TokenType.BLANK:
+            continue
+        if tkn.type == TokenType.BREAKLINE:
+            continue
+        if tkn.type == TokenType.DOUBLE_QUOTES:
+            continue
+        trimmed.append(tkn)
+    return trimmed
+
 @dataclass
 class ParseNode:
     '''node in parse tree of jadoo lang'''
-    
     def __post_init__(self):
-        parent : ParseNode = None
-        left : ParseNode = None
-        right : ParseNode = None
+        self.parent : ParseNode = None
+        self.child : list[ParseNode] = None
 
-    type : TokenType = None
+    type : TokenType = TokenType.NONE
     literal : str = None
     value : any = None
 
 @dataclass
 class ParseTree:
     '''parse tree of jadoo lang'''
+    def __post_init__(self):
+        self.node_root : ParseNode = ParseNode(type=TokenType.ROOT)
+        self.node_curr : ParseNode = None
+        self.tknlist : list[Token] = []
+        self.tkn_curr : int = None
 
-    root: ParseNode
+    def print(self, node: ParseNode) -> None:
+        '''print node in parse tree'''
+        if node:
+            for child_ in node.child:
+                self.parse(child_)
+                print(child_.type.name, end="->")
 
+# currently working
+###########################################################
     def parse(self, node: ParseNode) -> None:
         '''visit node in parse tree'''
-        if node:
-            self.parse(node.left)
-            print(node.data, end="->")
-            self.parse(node.right)
+        if self.tkn_curr == 0:
+            pass
 
-# parser
-def parser(line : list[Token]) -> list:
-    '''parser of jadoo lang'''
+        if node:
+            self.node_curr = node
+
+            if self.node_curr.type == TokenType.ROOT:
+                tmpnode = ParseNode()
+                tmpnode.parent = self.node_root
+                self.node_root.child.append(tmpnode)
+                self.node_curr = tmpnode
+            elif self.node_curr.type == TokenType.PRINT:
+                tmpnode = ParseNode()
+                tmpnode.parent = self.node_root
+                self.node_root.child.append(tmpnode)
+                self.node_curr = tmpnode
+
+            for child_ in node.child:
+                print(child_.type.name, end="->")
+                self.parse(child_)
+
+        self.tkn_curr += 1
+###########################################################
+
+# not started
+###########################################################
+    def build_dict(self, node: ParseNode) -> dict[Token]:
+        '''build dict for parse tree'''
+        return {node}
+###########################################################
 
 if __name__ == "__main__":
     with open("code.jadoo", "rt", encoding="utf8") as f:
         code_tokenized : list[Token] = []
         line_raw = f.readline()
         while line_raw:
+            for i in range(100):
+                print("=", end="")
+            print(f"\n\nline : <{line_raw.strip()}>", end="")
+            print("\n\nlexer : ", end="")
             line_tokenized = lexer(line_raw)
-            for tkn in line_tokenized:
-                print(f"[<{tkn.literal}>, {tkn.type.name}]", end=", ")
+            for token in line_tokenized:
+                print(f"[<{token.literal}>, {token.type.name}]", end=", ")
+            
+            print("\n\ntrimmer : ", end="")
+            line_trimmed = trimmer(line_tokenized)
+            for token in line_trimmed:
+                print(f"[<{token.literal}>, {token.type.name}]", end=", ")
                 # print(f"<{tkn.literal}>", end=", ")
-                code_tokenized.append(tkn)
+                code_tokenized.append(token)
+            print("\n\n", end="")
             line_raw = f.readline()
+
+    # not started
+    ###########################################################
+            print("parser : ")
+            parse_tree = ParseTree()
+            parse_tree.tknlist = code_tokenized
+            parse_tree.parse(parse_tree.node_root)
+            parse_tree.print(parse_tree.node_root)
+            # code_parsed = parse_tree.build_dict(parse_tree.node_root)
+            # print(code_parsed)
+    ###########################################################
